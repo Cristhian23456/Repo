9d3eaa0af031c2016bd9170ac72e124de5f1df06
Cristhian_Q <cristhianalbertoquizhpe@gmail.com>
2024-10-17 12:16:02 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto02/src/proyecto02/proyecto02.java
@@ -0,0 +1,13 @@
+
+package proyecto02;
+public class proyecto02 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java\n" +
+                            "Versionado a través de Git\n" +
+                            "Manejado desde GitHub\n" +
+                            "Además se usar GitHubClassroom6>");
+    }
+    
+}
+
