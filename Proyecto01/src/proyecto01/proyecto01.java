cc1a402637c98774efbe9d736128e478a27d55b8
Cristhian_Q <cristhianalbertoquizhpe@gmail.com>
2024-10-17 12:12:16 -0500

    Segundo commit

--- Proyecto01/src/proyecto01/proyecto01.java
+++ Proyecto01/src/proyecto01/proyecto01.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package proyecto01;
 
-/**
- *
- * @author User
- */
+package proyecto01;
 public class proyecto01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ejemplo en Java\n" +
+                            "Manejado desde gitHub\n" +
+                            "Version a traves de Git\n" +
+                            "Creado por<Cristhian23456>");
     }
     
 }
